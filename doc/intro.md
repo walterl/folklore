e3269b9dc66fa39d75b0e7bfb745d0331615ba26
Walter Leibbrandt <git@wrl.co.za>
2022-05-19 20:02:46 +0200

    Scaffolding by seancorfield/deps-new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to walterl/folklore
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/walterl/folklore_test.clj
@@ -0,0 +1,7 @@
+(ns walterl.folklore-test
+  (:require [clojure.test :refer :all]
+            [walterl.folklore :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
