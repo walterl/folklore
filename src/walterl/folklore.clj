d0c1c582da765f9a8ad7e1b64bbf61750166e901
Walter Leibbrandt <git@wrl.co.za>
2022-05-19 22:26:23 +0200

    Initial implementation

--- src/walterl/folklore.clj
+++ src/walterl/folklore.clj
@@ -1,6 +1,65 @@
 (ns walterl.folklore)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(defn- append
+  "Appends `x` to `vect`or or empty vector when nil.
+  
+  This is `conj` but creates a vector for nil `coll`."
+  [vect x]
+  (conj (or vect []) x))
+
+(defn- apply-rollback
+  [acc {:keys [rollback] :as step-info}]
+  (if-not (fn? rollback)
+    acc ; should never happen
+    (try
+      (-> (rollback acc)
+          (update ::rollbacks append step-info))
+      (catch Exception ex
+        (update acc ::rollback-errors append {:error ex, :step step-info})))))
+
+(defn- rollback-steps
+  [{:keys [::succeeded ::failed] :as result}]
+  (let [rollbacks (->> (into [failed] (reverse succeeded))
+                       (filter (comp fn? :rollback)))]
+    (reduce apply-rollback result rollbacks)))
+
+(defn- apply-step
+  [acc {:keys [step] :as step-info}]
+  (try
+    (-> (step acc)
+        (update ::succeeded append step-info))
+    (catch Exception ex
+      (reduced (assoc acc
+                      ::error ex
+                      ::failed step-info)))))
+
+(defn reduce-steps
+  "Reduce over `steps` with given initial map value `m`.
+
+  `steps` should be a sequence of maps, each with a `:step` key, and an
+  optional `:rollback` key. The values for both must be a function accepting a
+  single argument – a map returned by the preceding step – and returns a map –
+  that will be passed to the next step. The first `:step` function will be
+  called with `m`.
+  
+  `m` defaults to an empty map.
+
+  If a step fails (throws an error), all `:rollback` functions for steps up to
+  and including the failing are called. These _compensating transaction_
+  functions must be [**idempotent** and **retryable**][1].
+
+  The following keys will be used for recording saga progress and error handling:
+
+  * `:walterl.folklore/succeeded`: Sequence of successfully completed steps.
+  * `:walterl.folklore/failed`: The step that raised an unhandled `java.lang.Exception`.
+  * `:walterl.folklore/error`: The exception that was raised by the failing step.
+  * `:walterl.folklore/rollbacks`: Sequence of rolled back steps.
+  * `:walterl.folklore/rollback-errors: Unhandled exceptions raised in rollback functions.
+
+  [1]: https://www.baeldung.com/cs/saga-pattern-microservices#1-what-is-saga-architecture-pattern"
+  ([steps]
+   (reduce-steps steps {}))
+  ([steps m]
+   (let [{:keys [::error] :as result} (reduce apply-step m steps)]
+     (cond-> result
+       (some? error) (rollback-steps)))))
--- test/walterl/folklore_test.clj
+++ test/walterl/folklore_test.clj
@@ -1,7 +1,61 @@
 (ns walterl.folklore-test
-  (:require [clojure.test :refer :all]
-            [walterl.folklore :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [walterl.folklore :as fl]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn- conj-step
+  [step-id]
+  (fn [m] (update m :steps conj step-id)))
+
+(defn- disj-step
+  [step-id]
+  (fn [m] (update m :steps disj step-id)))
+
+(deftest reduce-steps-test
+  (let [steps [{::id ::create-order ; id's aren't required, but helps with debugging
+                :step #(assoc % :steps #{::create-order})
+                :rollback (disj-step ::create-order)}
+               {::id ::process-payment
+                :step (conj-step ::process-payment)
+                :rollback (disj-step ::process-payment)}
+               {::id ::update-inventory
+                :step (conj-step ::update-inventory)
+                :rollback (disj-step ::update-inventory)}
+               {::id ::deliver-order
+                :step (conj-step ::deliver-order)
+                :rollback (disj-step ::deliver-order)}]]
+    (testing "basic success case"
+      (let [result (fl/reduce-steps steps)]
+        (is (= #{::create-order ::process-payment ::update-inventory
+                 ::deliver-order}
+               (:steps result)))
+        (is (= steps
+               (::fl/succeeded result)))))
+    (testing "basic rollback"
+      (let [test-error-msg "Test error"
+            steps (assoc-in steps [2 :step]
+                            #(throw (ex-info test-error-msg {:data %})))
+            {:keys [::fl/error ::fl/failed ::fl/rollbacks] :as result}
+            (fl/reduce-steps steps)]
+        (is (= test-error-msg
+               (ex-message error)))
+        (is (empty? (:steps result))) ; rollbacks have disjoined all values
+        (is (= #{::create-order ::process-payment} ; :steps at the time of error
+               (-> error ex-data :data :steps)))
+        (is (= ::update-inventory
+               (::id failed)))
+        ;; notice reverse order below, and includes failing step ::update-inventory
+        (is (= [::update-inventory ::process-payment ::create-order]
+               (mapv ::id rollbacks)))))
+    (testing "rollback functions are optional"
+      (let [steps (-> steps
+                      (update 1 dissoc :rollback) ; ::process-payment step
+                      (assoc-in [2 :step] #(throw (ex-info "Test error" {}))))
+            result (try
+                     (fl/reduce-steps steps)
+                     (catch Exception _
+                       ::failed))]
+        (testing "doesn't throw due to missing :rollback"
+          (is (not= ::failed
+                    result)))
+        (is (= #{::process-payment} ; no rollback, so couldn't disj step value
+               (:steps result)))))))
